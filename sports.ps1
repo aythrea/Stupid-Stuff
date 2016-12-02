d893b2b7261578056b47dcb27881dd78c50bf631
aythrea <aythrea@gmail.com>
2016-12-02 15:21:17 -0600

    Initial Synce

---
+++ sports.ps1
@@ -0,0 +1,9 @@
+﻿$sports = 'Hockey!','Lacrosse!','Football!'
+$things = 'Touchdown!','GRANDSLAM!','NOTHING BUT NET!'
+
+$sport = $sports[(Get-Random -Maximum ([array]$sports).count)]
+$thing = $things[(Get-Random -Maximum ([array]$things).count)]
+
+write-output "Go $sport do the $thing"
+ 
+ 
